ece069775cd4038d66a1a7c5692a6461b3d98d21
Francesco Messina <iframe@live.it>
2023-12-13 00:12:38 +0100

    fix: int to long int

--- 2023/C/05/second.c
+++ 2023/C/05/second.c
@@ -1,19 +1,20 @@
 #include "../inc/utils.h"
 
 int main ( void ) {
-    // int time = 59707878;
-    int time = 71530;
-    // size_t distance = 430121812131276;
-    size_t distance = 940200;
+    int time = 59707878;
+    // long int time = 71530;
+    size_t distance = 430121812131276;
+    // long int distance = 940200;
 
     size_t counter = 0;
-    int i,s = 0;
+    long int i = 0;
+    long int s = 0;
 
-    while(i <= time){
+    while(i < time){
         s = i * (time - i);
         if( s > distance)
             counter++;
         i++;
     }
-    printf("Solution %li", counter);
+    printf("Solution is %li", counter);
 }
