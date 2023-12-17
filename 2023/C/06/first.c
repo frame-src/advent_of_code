7ad6afafe11af4e778d524056d9f6ffd1dd276c7
Francesco Messina <iframe@live.it>
2023-12-17 22:42:51 +0100

    to complete

--- 2023/C/06/first.c
+++ 2023/C/06/first.c
@@ -40,8 +40,6 @@ enum hand_type calculate_value(size_t value, size_t counter){
 
 struct card_info *check_hand(struct card_info *ci){
     size_t i,j, counter;
-    char trio = '\0';
-    char duo = '\0';
     size_t value;
 
     i = j = counter = 0;
@@ -61,12 +59,10 @@ struct card_info *check_hand(struct card_info *ci){
         j = 0;
         i++;
     }
-
 }
 
 size_t *seeds = NULL;
 struct info **conversion_table = NULL;
-bool CONVERTING = false;
 
 void print(){
     int i = 0;
@@ -107,131 +103,12 @@ size_t *add_value_to_nmbrs(size_t *n, size_t value){
     return new;
 }
 
-void update_conversion_table(size_t *nmbrs){
-    int i = 0;
-    int size = 2;
-    while(conversion_table && conversion_table[i])
-        i++;
-    size = size + i;
-    struct info **new = malloc (sizeof(struct info *) * (size));
-    if(!new)
-        exit(1);
-    if(conversion_table){
-        for(i = 0; i < size - 2; i++){
-            new[i] = malloc(sizeof(struct info ));
-            if (!new[i])
-                exit(1);
-            new[i]->destination_start = conversion_table[i]->destination_start;
-            new[i]->source_start = conversion_table[i]->source_start;
-            new[i]->range = conversion_table[i]->range;
-            free(conversion_table[i]);
-        }
-        free(conversion_table);
-    }
-    new[i] = malloc(sizeof(struct info ) * 1);
-    if (!new[i])
-        exit(1);
-    new[i]->destination_start = nmbrs[0];
-    new[i]->source_start = nmbrs[1];
-    new[i]->range = nmbrs[2];
-    new[i + 1] = NULL;
-    conversion_table = new;
-}
-
-void fill_conversion_table(char* line){
-    size_t i = 0;
-    size_t value = 0;
-    size_t *nmbrs = NULL;
-
-    while(line[i]){
-        while(line[i] && !isnumber(line[i]))
-            i++;
-        value = atol(&line[i]);
-        nmbrs = add_value_to_nmbrs(nmbrs, value);
-        while(isnumber(line[i]))
-            i++;
-        i++;
-    }
-    if(!seeds){
-        seeds = nmbrs;
-        return;
-    }
-    update_conversion_table(nmbrs);
-    free(nmbrs);
-}
-
-size_t convert(size_t seed){
-    size_t i = 0;
-    size_t upper_bound = 0;
-    size_t new_value = 0;
-    bool check = false;
-    while(conversion_table[i]){
-        upper_bound = conversion_table[i]->source_start + conversion_table[i]->range;
-        if(conversion_table[i]->source_start <= seed && seed < upper_bound){
-            check = true;
-            break;
-        }
-        i++;
-    }
-    if(check == true){
-        size_t diff = seed - conversion_table[i]->source_start;
-        new_value = conversion_table[i]->destination_start + diff;
-    }
-    else 
-        new_value = seed;
-    return new_value;
-}
-
-void perform_conversion(){
-    size_t i = 0 ;
-    if(conversion_table){
-        while(seeds[i] > 0){
-            seeds[i] = convert(seeds[i]);
-            i++;
-        }
-    }
-}
-
-void clean_conversion_table(){
-    int i = 0;
-    
-    if( conversion_table){
-        while( conversion_table[i]){
-            free( conversion_table[i]);
-            i++;
-        }
-        free(conversion_table);
-        conversion_table = NULL;
-    }
-}
-
 void process_line(char *line){
-    if(isalpha(line[0])){
-        clean_conversion_table();
-        CONVERTING = true;
-        return;
-    }
-    else if(line[0] == '\n' || line[0] == '\0'){
-        perform_conversion();
-        CONVERTING = false;
-        return;
-    } else {
-        fill_conversion_table(line);
-    }
+    first_part(&line[0]);
+    second_part(&line[5]);
 }
 
-size_t min_sort(){
-    int i = 0;
-    size_t min = seeds[0];
-    while(seeds[i]){
-        if(seeds[i] < min){
-            min = seeds[i];
-            i = 0;
-        }
-        i++;
-    }
-    return min;
-}
+
 
 void calculate_value(char *buffer)
 {
@@ -245,7 +122,6 @@ void calculate_value(char *buffer)
         line = get_line(&buffer[i]);
     }
     i = 0;
-    printf("%li",min_sort());
 }
 
 int main (void)
