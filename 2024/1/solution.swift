f669806bcfdf3d680e4bf555c3551d4064495721
Francesco Messina <iframe@live.it>
2024-12-02 16:26:19 +0100

    feat: content split into two array to do: order them and calculate distance

--- 2024/1/solution.swift
+++ 2024/1/solution.swift
@@ -17,14 +17,33 @@ func readFileContent(from fileName: String, at directoryPath: String) -> String 
     }
 }
 
+func splitContent(input: String) -> ([Int], [Int]) {
+    let lines = input.split(separator: "\n")
+    var left : [Int] = []
+    var right : [Int] = [] 
+
+    for line in lines {
+        let numbers = line.split(separator: " ")
+        if let leftNumber = Int(numbers[0]), let rightNumber = Int(numbers[1]) {
+            left.append(leftNumber)
+            right.append(rightNumber)
+        }
+    }
+    return (left,right)
+}
+
 func main() {
     let fileName = "input"
+
     let fileManager = FileManager.default
     let currentDirectoryPath = fileManager.currentDirectoryPath
     var content : String = ""
     content = readFileContent(from: fileName, at: currentDirectoryPath)
-    print (content)
-    
+
+    let (left, right) = splitContent( input:content )
+    print (left)
+    print (right)
+
 }
 
 main()
